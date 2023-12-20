312db2ba76bf2dbcd919b61001c7df6a769892ae
Eberson Oliveira <eberson.olivera@gmail.com>
2023-12-19 22:15:02 -0300

    added toml's first version

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    `version-catalog`
+    `maven-publish`
+}
+
+group = "br.com.fiap.tech.challenge"
+version = "1.0.0"
+
+catalog {
+    versionCatalog {
+        from(files("gradle/libs.versions.toml"))
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["versionCatalog"])
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/fiap-postech/gradle-version-catalog")
+            credentials {
+                username = findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
+                password = findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
